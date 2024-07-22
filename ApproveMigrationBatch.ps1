6b9071a75a0b1c62f2fe4c4d6407c5d9e402c776
Reggie Wilson <wilsonrg5338@gmail.com>
2024-07-22 01:02:48 -0500

    Add files via upload

---
+++ ApproveMigrationBatch.ps1
@@ -0,0 +1,10 @@
+﻿#Author: Reggie Wilson
+#Last Modified: 5/10/24
+
+Connect-ExchangeOnline
+
+#Approve Migration Batch 1
+#Set-MigrationUser -Identity "email address removed for privacy reasons" -ApproveSkippedItems​
+
+#Approve Migration Batch 2
+Get-MigrationBatch -Status needsapproval | Set-MigrationBatch -ApproveSkippedItems
